a371cd4bfd65fa57304071af2ec8298cb05aa53b
ganeshjeeray000 <85294710+ganeshjeeray000@users.noreply.github.com>
2021-07-19 19:01:29 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,17 @@
+class Bird extends Baseclass
+{
+    constructor(x, y)
+    {
+        super(x,y,50,50);// calling the Baseclass(parent)constructor function
+       this.image = loadImage("sprites/bird.png");
+
+    }
+
+    display()
+    {
+        this.body.position.x = mouseX;
+        this.body.position.y = mouseY;
+        
+     super.display(); //display()function of the parent class is going to be called or invoked
+    }
+};
